fb09364789c253142c6154880dc02bb9dbb06018
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2026-05-23 11:52:59 +0200

    chore: kamil-setup addes server.js fle

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+
+// ROUTES
+app.get("/", (req, res) => {
+    res.send("Backend is running...")
+})
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+})

bb2d8a3873f501973d7b0e7bd7b5bd27725cff2e
ArnoBaeck <145569200+ArnoBaeck@users.noreply.github.com>
2026-05-23 14:18:26 +0200

    refactor: server file with better error handling

--- server.js
+++ server.js
@@ -3,17 +3,34 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // ROUTES
 app.get("/", (req, res) => {
-    res.send("Backend is running...")
-})
+	res.send("Backend is running...");
+});
 
 app.use("/api/check-url", require("./routes/checkRoute"));
+
+app.use((req, res) => {
+	res.status(400).json({
+		error: "Endpoint not found",
+		path: req.originalUrl,
+		tip: "Please check backend to see if this endpoint exists",
+	});
+});
+
+app.use((err, req, res, next) => {
+	console.error("Unexpected crash: ", err.stack);
+	res.status(500).json({
+		error: "Internal server error",
+		message: err.message,
+	});
+});
 
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT);
-})
+	console.log("Server is running on port " + PORT);
+});
